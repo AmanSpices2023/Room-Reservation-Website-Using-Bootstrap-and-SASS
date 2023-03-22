06194eca193d6f76f2f9d04aab78ab73f85873a1
Spice Aman Karanjit <aman.spices2023@gmail.com>
2023-03-22 17:28:49 +0545

    first commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 15,      
+  });
+var swiper = new Swiper(".testimonialSwiper", {
+    slidesPerView: 2,
+    spaceBetween: 15,      
+  });
